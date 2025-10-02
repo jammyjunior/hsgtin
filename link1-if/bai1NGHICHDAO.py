6e2029fc76330311e28a593733a8906c4ad29fb9
Jammy Jellyfish <trannguyenminhkhang0000@gmail.com>
2025-10-02 20:54:28 +0700

    first commit

---
+++ link1-if/bai1NGHICHDAO.py
@@ -0,0 +1,6 @@
+a = int(input())
+if a != 0:
+    b = 1/a
+    print(f"{b:,.4f}")
+else:
+    print("khong co")
---
+++ link1-if/bai2AMDUONG.py
@@ -0,0 +1,8 @@
+a = int(input())
+
+if a>0:
+    print(f"{a} la so duong")
+elif a==0:
+    print(f"{a} la so khong am khong duong")
+else:
+    print(f"{a} la so am")
